1fbf617915e45ab41698fcd012f8c045960f5ffd
Ssdosaofc <147481143+Ssdosaofc@users.noreply.github.com>
2024-05-16 01:29:03 +0530

    commit

--- app/src/main/java/com/example/musical/ui/tabla/TablaFragment.kt
+++ app/src/main/java/com/example/musical/ui/tabla/TablaFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.musical.databinding.FragmentGalleryBinding
@@ -28,10 +27,6 @@ class TablaFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        pianoViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
